f61bac5086997ed2fd925609f953a85dd8513f95
FredSt0ne <fredstone624@gmail.com>
2019-03-02 17:48:52 +0300

    completed tasks 03-date

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -21,7 +21,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-  throw new Error('Not implemented');
+  return Date.parse(value);
 }
 
 /**
@@ -36,7 +36,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-  throw new Error('Not implemented');
+  return Date.parse(value);
 }
 
 
@@ -55,7 +55,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-  throw new Error('Not implemented');
+  const year = date.getFullYear();
+
+  return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0);
 }
 
 
@@ -75,7 +77,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-  throw new Error('Not implemented');
+  return new Date(endDate - startDate).toISOString().slice(11, -1); 
 }
 
 
@@ -94,7 +96,9 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-  throw new Error('Not implemented');
+  const angle = 0.5 * (60 * date.getUTCHours() - 11 * date.getUTCMinutes());
+
+  return (angle > 180 ? Math.abs(angle - 360 * Math.round(angle / 360)) : angle) * Math.PI / 180;
 }
 
 module.exports = {
